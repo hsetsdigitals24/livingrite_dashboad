b56b696c3e691fd01e511c65104847985f27cb35
Victor Olorunda <victorolorunda6@gmail.com>
2026-02-08 23:19:19 +0100

    fixed schedule page

---
+++ app/caregiver/schedule/page.tsx
@@ -0,0 +1,12 @@
+
+
+const Schedule = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Schedule</h1>
+      <p>This is the schedule page for caregivers.</p>
+    </div>
+  );
+}
+
+export default Schedule;

bb1caca77a49f7238d77cb5771496e425a4fe473
Victor Olorunda <victorolorunda6@gmail.com>
2026-02-08 23:16:40 +0100

    vercel file fixed

---
+++ app/caregiver/clients/page.tsx
@@ -0,0 +1,11 @@
+
+const Clients = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Clients</h1>
+      <p>This is the clients page for caregivers.</p>
+    </div>
+  );
+}
+
+export default Clients;
